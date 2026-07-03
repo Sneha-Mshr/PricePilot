79926eca9d2cc83b5de376ca8781e84eed8c5938
Sneha <snehamishra132003@gmail.com>
2026-07-03 14:08:06 +0530

    :tada: congratulations

---
+++ spring-backend/backend/src/main/java/com/pricepilot/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pricepilot.backend.repository;
+
+import com.pricepilot.backend.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
